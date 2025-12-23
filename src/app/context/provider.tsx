e659d59a543400fbe0d959b50ff8bf97ac7352d6
Suresh-Kannan-8055 <sureshkannangeek@gmail.com>
2025-12-23 22:58:47 +0530

    First Commit

--- src/app/context/provider.tsx
+++ src/app/context/provider.tsx
@@ -1,11 +1,15 @@
 'use client';
 import { HeroUIProvider } from '@heroui/react'
 import { PropsWithChildren } from 'react'
+import { ThemeProvider } from 'next-themes';
+
 
 export default function Provider({ children }: PropsWithChildren) {
     return (
         <HeroUIProvider>
-            {children}
+            <ThemeProvider attribute="class" defaultTheme="dark">
+                {children}
+            </ThemeProvider>
         </HeroUIProvider>
     )
 }
---
+++ src/app/generate/[id]/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Page() {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  return (
+    <>
+      
+    </>
+  );
+}
